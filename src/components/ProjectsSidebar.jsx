96e57db5f2e26d459ccc4dcf4da689953cba86bf
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-05-16 13:37:07 +0200

    fix

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -12,7 +12,9 @@ export default function ProjectsSidebar({ handleAddProject, projects }) {
         {projects.map((project) => {
           return (
             <li key={project.id} className="flex justify-between my-4">
-              {project.title}
+              <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+                {project.title}
+              </button>
             </li>
           );
         })}
